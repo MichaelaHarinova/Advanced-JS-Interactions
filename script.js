16d5edaed359272109e5cd1bc3f656c367c72cd7
Micha <michaelaharin@gmail.com>
2021-05-10 09:02:01 +0200

    refactored code for runner

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /* dark mode switch */
 document.querySelector(".mode").addEventListener("click", switchToDark);
 
@@ -24,6 +23,7 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 2500);
 }
+
 showSlides();
 
 /* cards collage */
@@ -87,53 +87,49 @@ let runner = document.querySelector('.runner');
 let box2 = document.querySelector(".box2");
 let xr = 0;
 let yr = 0;
+let insideBox = false;
 
 box2.addEventListener('mousemove', mouseMoveRun, true);
+box2.addEventListener('mouseleave', () => {
+    insideBox = false;
+})
 
 function mouseMoveRun(e) {
+    insideBox = true;
     xr = e.pageX;
     yr = e.pageY;
 
-    if (countDistance().x >= 25 && countDistance().x <= 50 && countDistance().y >= -50 && countDistance().y <= -25) {
-        console.log(runner.style.left);
-        runner.style.left = (parseInt(getComputedStyle(runner).left) - 10) + 'px';
-        runner.style.top = (parseInt(getComputedStyle(runner).top) + 10) + 'px';
+    if (xr <= 1160 && yr <= 2900) {
+        if (countDistance().x >= 25 && countDistance().x <= 50 && countDistance().y >= -50 && countDistance().y <= -25) {
+            runner.style.left = (parseInt(getComputedStyle(runner).left) - 10) + 'px';
+            runner.style.top = (parseInt(getComputedStyle(runner).top) + 10) + 'px';
+        } else if (countDistance().x >= 0 && countDistance().x <= 50 && countDistance().y >= -25 && countDistance().y <= 25) {
+            runner.style.left = (parseInt(getComputedStyle(runner).left) - 10) + 'px';
+        } else if (countDistance().x >= 25 && countDistance().x <= 50 && countDistance().y >= 25 && countDistance().y <= 50) {
+            runner.style.left = (parseInt(getComputedStyle(runner).left) - 10) + 'px';
+            runner.style.top = (parseInt(getComputedStyle(runner).top) - 10) + 'px';
+        } else if (countDistance().x >= -25 && countDistance().x <= 25 && countDistance().y >= 0 && countDistance().y <= 50) {
+            runner.style.top = (parseInt(getComputedStyle(runner).top) - 10) + 'px';
+        } else if (countDistance().x >= -50 && countDistance().x <= -25 && countDistance().y >= 25 && countDistance().y <= 50) {
+            runner.style.left = (parseInt(getComputedStyle(runner).left) + 10) + 'px';
+            runner.style.top = (parseInt(getComputedStyle(runner).top) - 10) + 'px';
+        } else if (countDistance().x >= -50 && countDistance().x <= 0 && countDistance().y >= -25 && countDistance().y <= 25) {
+            runner.style.left = (parseInt(getComputedStyle(runner).left) + 10) + 'px';
+        } else if (countDistance().x >= -50 && countDistance().x <= -25 && countDistance().y >= -50 && countDistance().y <= -25) {
+            runner.style.left = (parseInt(getComputedStyle(runner).left) + 10) + 'px';
+            runner.style.top = (parseInt(getComputedStyle(runner).top) + 10) + 'px';
+        } else if (countDistance().x >= -25 && countDistance().x <= 25 && countDistance().y >= -50 && countDistance().y <= 0) {
+            runner.style.top = (parseInt(getComputedStyle(runner).top) + 10) + 'px';
+        }
     }
-    else if (countDistance().x >= 0 && countDistance().x <= 50 && countDistance().y >= -25 && countDistance().y <= 25) {
-        runner.style.left = (parseInt(getComputedStyle(runner).left) - 10) + 'px';
-     //  runner.style.top = (parseInt(getComputedStyle(runner).top) + 10) + 'px';
-    }
-    else if (countDistance().x >= 25 && countDistance().x <= 50 && countDistance().y >= 25 && countDistance().y <= 50) {
-        runner.style.left = (parseInt(runner.style.left) - 10) + 'px';
-        runner.style.top = (parseInt(runner.style.top) - 10) + 'px';
-    }
-    else if (countDistance().x >= -25 && countDistance().x <= 25 && countDistance().y >= 0 && countDistance().y <= 50) {
-      // runner.style.left = (parseInt(runner.style.left) - 10) + 'px';
-        runner.style.top = (parseInt(runner.style.top) - 10) + 'px';
-    }
-    else if (countDistance().x >= -50 && countDistance().x <= -25 && countDistance().y >= 25 && countDistance().y <= 50) {
-        runner.style.left = (parseInt(runner.style.left) + 10) + 'px';
-        runner.style.top = (parseInt(runner.style.top) - 10) + 'px';
-    }
-    else if (countDistance().x >= -50 && countDistance().x <= 0 && countDistance().y >= -25 && countDistance().y <= 25) {
-        runner.style.left = (parseInt(runner.style.left) + 10) + 'px';
-     //   runner.style.top = (parseInt(runner.style.top) - 10) + 'px';
-    }
-      else if (countDistance().x >= -50 && countDistance().x <= -25 && countDistance().y >= -50 && countDistance().y <= -25) {
-        runner.style.left = (parseInt(runner.style.left) + 10) + 'px';
-        runner.style.top = (parseInt(runner.style.top) + 10) + 'px';
-    }
-  //  console.log(runner.style.left)
 }
 
 function countDistance() {
     return {
         x: xr - parseInt(getComputedStyle(runner).left) - 25,
-        y: yr - parseInt(getComputedStyle(runner).top)  - 25
+        y: yr - parseInt(getComputedStyle(runner).top) - 25
     };
 }
-
-
 
 
 /* games - chaser/ another solution */
@@ -177,12 +173,12 @@ let letters = document.querySelectorAll('.letter');
 let newString = '';
 box.addEventListener('keydown', removeCharacter);
 
-    function removeCharacter() {
-        newString = box.replace('g', '');
+function removeCharacter() {
+    newString = box.replace('g', '');
 
-        document.querySelector('.letter').textContent
-            = newString;
-    }
+    document.querySelector('.letter').textContent
+        = newString;
+}
 
 
 
